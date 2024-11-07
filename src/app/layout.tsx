27f26febd843e33e888748dcfc8360439b96016d
zkz <2358305598@qq.com>
2024-11-07 16:30:13 +0900

    万能刷 完善权限校验 整合Markdown编辑器 全局状态管理器 创建Axios类 封装请求方法

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { Provider, useDispatch } from "react-redux";
 import { getLoginUserUsingGet } from "@/api/userController";
 import { setLoginUser } from "@/stores/loginUser";
 import AccessLayout from "@/access/AccessLayout";
-import accessEnum from "@/access/accessEnum";
 
 const InitLayout: React.FC<
   Readonly<{
@@ -22,11 +21,6 @@ const InitLayout: React.FC<
     if (res.data) {
       dispatch(setLoginUser(res.data));
     } else {
-      // todo 测试代码，实际可删除
-      setTimeout(() => {
-        const testUser = { userName: "测试登录", id: 1 ,userRole: accessEnum.ADMIN};
-        dispatch(setLoginUser(testUser));
-      }, 3000);
     }
   }, [dispatch]);
   useEffect(() => {
